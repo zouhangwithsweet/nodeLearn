a1caf4a5728062d83a656de6dac8069ac3895040
zouhang <1102639563@qq.com>
2018-02-11 14:43:13 +0800

    mm

--- src/config/index.js
+++ src/config/index.js
@@ -2,7 +2,7 @@ module.exports = {
     root: process.cwd(),
     hostname: '127.0.0.1',
     port: '9527',
-    compress: /\.(tpl|html|css|md|js)/,
+    compress: /\.(tpl|html|css|md|js|json)/,
     cache: {
         maxAge: 600,
         cacheControl: true,
