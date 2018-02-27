eb4226a5cbe29f82493a7358363c100c2e63aa00
zouhang <1102639563@qq.com>
2018-02-27 21:19:34 +0800

    bin

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ const http = require('http')
 const conf = require('./config')
 const path = require('path')
 const readF = require('./helper/route')
+const openUrl = require('./helper/open')
 
 class Server {
     constructor(config) {
@@ -16,6 +17,7 @@ class Server {
         server.listen(this.conf.port, this.conf.hostname, () => {
             const addr = `http://${this.conf.hostname}:${this.conf.port}`
             console.log(`server is on ${addr}`)
+            openUrl(addr)
         })
     }
 }
