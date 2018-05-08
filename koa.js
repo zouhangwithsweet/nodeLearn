dd47214b3d2440e54a73c91ba98eb45625b066ad
zouhang <1102639563@qq.com>
2018-05-08 22:10:41 +0800

    模板引擎

--- koa.js
+++ koa.js
@@ -1,15 +1,53 @@
-const Koa = require('koa')
-const router = require('koa-router')()
-const bodyParser = require('koa-bodyparser')
-const fs = require('fs')
-const controller = require('./controller')
-const app = new Koa()
+// const Koa = require('koa')
+// const router = require('koa-router')()
+// const bodyParser = require('koa-bodyparser')
+// const fs = require('fs')
+// const controller = require('./controller')
+// const app = new Koa()
 
+const nunjucks = require('nunjucks');
 
-app.use(bodyParser())
+function createEnv(path, opts) {
+    var
+        autoescape = opts.autoescape === undefined ? true : opts.autoescape,
+        noCache = opts.noCache || false,
+        watch = opts.watch || false,
+        throwOnUndefined = opts.throwOnUndefined || false,
+        env = new nunjucks.Environment(
+            new nunjucks.FileSystemLoader('views', {
+                noCache: noCache,
+                watch: watch,
+            }), {
+                autoescape: autoescape,
+                throwOnUndefined: throwOnUndefined
+            });
+    if (opts.filters) {
+        for (var f in opts.filters) {
+            env.addFilter(f, opts.filters[f]);
+        }
+    }
+    return env;
+}
 
-// add router middleware
-app.use(controller())
+var env = createEnv('views', {
+    watch: true,
+    filters: {
+        hex: function (n) {
+            return '0x' + n.toString(16);
+        }
+    }
+});
+var s = env.render('hello.html', { name: '小明' });
+console.log(s);
+console.log(env.render('extend.html', {
+    header: 'Hello',
+    body: 'bla bla bla...'
+}));
 
-app.listen(9527)
-console.log('server start up')
+// app.use(bodyParser())
+
+// // add router middleware
+// app.use(controller())
+
+// app.listen(9527)
+// console.log('server start up')
